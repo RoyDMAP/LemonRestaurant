af81a7eb9267d43e680c67bd713ee5b6eb3c08fb
RoyDMAP <roy.dimapilis@yahoo.com>
2025-07-04 11:16:42 -0700

    Initial Commit

---
+++ LemonRestaurant/LemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantApp.swift
+//  LemonRestaurant
+//
+//  Created by Roy Dimapilis on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
